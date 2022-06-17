8ddefcfa7b879c4613a31c36c0e194536013e940
bansighetiya <bansighetiya7@gmail.com>
2022-06-17 15:19:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import  React from 'react';
+import  { Route, Switch } from 'react-router-dom';
+import Header from './Component/Header/Header';
+import Footer from './Component/Footer/Footer';
+import Home from './Container/Home/Home';
+import About from './Container/About/About';
+import Product from './Container/Product/Product';
+import Blog_list from './Container/Blog_list/Blog_list';
+import Contant from './Container/Contant/Contant';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    <Header />
+
+    <Switch>
+
+      <Route path={"/"} exact component={Home} />
+      <Route path={"/About"} exact component={About} />
+      <Route path={"/product"} exact component={Product} />
+      <Route path={"/blog"} exact component={Blog_list} />
+      <Route path={"/contact"} exact component={Contant} />
+      
+    </Switch>
+
+    <Footer />
+
+    </>
   );
 }
 
---
+++ src/Container/Testimonial/Testimonial.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Testimonial(props) {
+    return (
+       <div>
+        
+       </div>
+    );
+}
+
+
+export default Testimonial;
